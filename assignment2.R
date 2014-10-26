23e0aed48178493e70277fda29e4c26731c28605
cesine <cesine@yahoo.com>
2014-10-26 19:31:42 -0400

    notes

--- assignment2.R
+++ assignment2.R
@@ -34,3 +34,25 @@ cachemean <- function(x, ...) {
     x$setmean(m)
     m
 }
+
+########
+# Matrix inversion is usually a costly computation and 
+# their may be some benefit to caching the inverse of a matrix 
+# rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). 
+########
+
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(){
+
+}
+
+
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+#  If the inverse has already been calculated (and the matrix has not changed), 
+#  then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(){
+	# Computing the inverse of a square matrix can be done with the solve function in R. 
+	# For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+
+
+}
