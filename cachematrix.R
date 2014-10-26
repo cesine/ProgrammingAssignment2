d2fba022f5f2c49659b7defa72a58af610b23038
cesine <cesine@yahoo.com>
2014-10-26 19:55:36 -0400

    realized that the functions were laid out as we needed for the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,39 @@
+debugging = TRUE
 
-# The first function, makeVector creates a special "vector", which is really a list containing a function to
-# 	set the value of the vector
-# 	get the value of the vector
-# 	set the value of the mean
-# 	get the value of the mean
-makeVector <- function(x = numeric()) {
+
+# The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+# 	set the value of the matrix
+# 	get the value of the matrix
+# 	set the value of the inverse
+# 	get the value of the inverse
+makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
             x <<- y
             m <<- NULL
     }
     get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
     list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-# The following function calculates the mean of the special "vector" created with the above function. 
-# However, it first checks to see if the mean has already been calculated. 
-# If so, it gets the mean from the cache and skips the computation. 
-# Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
-cachemean <- function(x, ...) {
-    m <- x$getmean()
+# The following function calculates the inverse of the special "matrix" created with the above function. 
+# However, it first checks to see if the inverse has already been calculated. 
+# If so, it gets the inverse from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinverse function.
+cacheSolve <- function(x, ...) {
+    m <- x$getinverse()
     if(!is.null(m)) {
             message("getting cached data")
             return(m)
     }
     data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
+    m <- solve(data, ...)
+    x$setinverse(m)
     m
 }
 
@@ -42,23 +44,32 @@ cachemean <- function(x, ...) {
 ########
 
 # makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-makeCacheMatrix <- function(x = matrix()){
-	specialMatrix <-  data.frame("matrix" = x, "inverse" = matrix()) 
+# makeCacheMatrix <- function(x = matrix()){
+# 	specialMatrix <-  data.frame("matrix" = x, "inverse" = matrix()) 
 
-	specialMatrix
-}
+# 	specialMatrix
+# }
 
 
-# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-#  If the inverse has already been calculated (and the matrix has not changed), 
-#  then the cachesolve should retrieve the inverse from the cache.
-cacheSolve <- function(x, ...){
-	# Computing the inverse of a square matrix can be done with the solve function in R. 
-	# For example, if X is a square invertible matrix, then solve(X) returns its inverse.
-	if(!x$inverse){
-		x$inverse <- solve(x)
-	}
+# # cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# #  If the inverse has already been calculated (and the matrix has not changed), 
+# #  then the cacheSolve should retrieve the inverse from the cache.
+# cacheSolve <- function(x, ...){
+# 	# Computing the inverse of a square matrix can be done with the solve function in R. 
+# 	# For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+# 	if(is.null(x$inverse)){
+# 		if(debugging){
+# 			print("Calculating the inverse matrix")
+# 		}
+# 		x$inverse <- solve(x)
+# 	}else{
+# 		if(debugging){
+# 			print("The inverse matrix was already calculated")
+# 		}
+# 	}
 	
-	## Return a matrix that is the inverse of 'x'
-	x$inverse 
-}
+# 	## Return a matrix that is the inverse of 'x'
+# 	x$inverse 
+# }
+
+
